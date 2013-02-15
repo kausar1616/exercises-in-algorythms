d836b80813082f6054b4c63322eeebe9b4f9993c
boyan.palagachev@gmail.com <boyan.palagachev@gmail.com@238ea0c3-21ee-db24-a851-29f3e7fd01f4>
2013-02-15 01:08:01 +0000

    Some simple recursion examples added

---
+++ trunk/ exercises-in-algorythms --username boyan.palagachev@gmail.com/03. Recursion/01. BasicRecursionExamples/BasicRecursionExamples.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class BasicRecursionExamples
+{
+    static void Main()
+    {
+        //Console.WriteLine(Factorial(5));
+        Console.WriteLine(Fibonacci(11));
+    }
+
+    // Simple program to illustrate the basic features of recursive program
+    static int Factorial(int num)
+    {
+        if (num == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * Factorial(num - 1);
+        }
+    }
+    // Very ineffective. Recursion should not be used here. Extensive recomputing!
+    static int Fibonacci(int num)
+    {
+        if (num <=1 )
+        {
+            return 1;
+        }
+        else
+        {
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+    }
+}
